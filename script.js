8e32e56da2e14e748c87941a9ede0290e7fa8b76
codinglil <102619991+codinglil@users.noreply.github.com>
2022-09-20 17:08:06 -0400

    created the sound board html, css, js

---
+++ script.js
@@ -0,0 +1,21 @@
+const sounds = ["airport", "clapping", "birds", "game", "running", "surprised"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSong();
+    document.getElementById(sound).play();
+  });
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSong() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
